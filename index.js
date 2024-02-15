9b762935e9d88ed5cb44af65085ca47d87e48a73
Priya Dharshini <154991539+project-std@users.noreply.github.com>
2024-02-15 16:49:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+let button =document.getElementById('btn');
+button.addEventListener('click',dateAndTime);
+function dateAndTime(){
+  // alert("clicked");
+  let now = new Date();
+  let year = now.getFullYear();
+  let month = now.getMonth()+1;
+  let dateValue = now.getDate();
+  let hour = now.getHours();
+  let min = now.getMinutes();
+  let sec = now.getSeconds();
+  let show = document.getElementById('result');
 
+  show.innerHTML = `Month = ${month} <br> Year = ${year} <br>  Date = ${dateValue} <br> Hour = ${hour} <br> Minutes = ${min} <br> Seconds = ${sec} <br>`;
+  
+}
